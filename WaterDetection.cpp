ffa1ff372c7e0420494423201b8fbc8f730ff52f
Harsh P Bajaj <harsh.rocks@hotmail.com>
2017-05-02 01:50:59 +0530

    Update WaterDetection.cpp

--- WaterDetection.cpp
+++ WaterDetection.cpp
@@ -8,270 +8,88 @@
 
 using namespace cv;
 using namespace std;
-
-
-Mat markerMask, img;
-Point prevPt(-1, -1);
-
-void mergeSegments(Mat & image,Mat & segments, int & numOfSegments)
+Mat applyCustomColorMap(Mat& im_gray)
 {
-	//To collect pixels from each segment of the image
-	vector<Mat> samples;
-	//In case of multiple merging iterations, the numOfSegments should be updated
-	int newNumOfSegments = numOfSegments;
-	
-	//Initialize the segment samples
-	for(int i=0;i<=numOfSegments;i++)
-	{
-		Mat sampleImage;
-		samples.push_back(sampleImage);
-	}
-	
-	//collect pixels from each segments
-	for(int i = 0; i < segments.rows; i++ )
-	{
-		for(int j = 0; j < segments.cols; j++ )
-		{
-			//check what segment the image pixel belongs to
-			int index = segments.at<int>(i,j);
-			if(index >= 0 && index<numOfSegments)
-			{
-				samples[index].push_back(image(Rect(j,i,1,1)));	
-			}
-		}
-	}
-	
-	//create histograms
-	vector<MatND> hist_bases;
-	Mat hsv_base;
-	/// Using 35 bins for hue component 
-	int h_bins = 35;
-	/// Using 30 bins for saturation component
-	int s_bins = 30;
-	int histSize[] = { h_bins,s_bins };
+    unsigned char b[] = {255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,253,251,249,247,245,242,241,238,237,235,233,231,229,227,225,223,221,219,217,215,213,211,209,207,205,203,201,199,197,195,193,191,189,187,185,183,181,179,177,175,173,171,169,167,165,163,161,159,157,155,153,151,149,147,145,143,141,138,136,134,132,131,129,126,125,122,121,118,116,115,113,111,109,107,105,102,100,98,97,94,93,91,89,87,84,83,81,79,77,75,73,70,68,66,64,63,61,59,57,54,52,51,49,47,44,42,40,39,37,34,33,31,29,27,25,22,20,18,17,14,13,11,9,6,4,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 
-	// hue varies from 0 to 256, saturation from 0 to 180
-	float h_ranges[] = { 0, 256 };	
-	float s_ranges[] = { 0, 180 };
 
-	const float* ranges[] = { h_ranges, s_ranges };
+    unsigned char g[] = { 255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,254,252,250,248,246,244,242,240,238,236,234,232,230,228,226,224,222,220,218,216,214,212,210,208,206,204,202,200,198,196,194,192,190,188,186,184,182,180,178,176,174,171,169,167,165,163,161,159,157,155,153,151,149,147,145,143,141,139,137,135,133,131,129,127,125,123,121,119,117,115,113,111,109,107,105,103,101,99,97,95,93,91,89,87,85,83,82,80,78,76,74,72,70,68,66,64,62,60,58,56,54,52,50,48,46,44,42,40,38,36,34,32,30,28,26,24,22,20,18,16,14,12,10,8,6,4,2,0 };
 
-	// Use the 0-th and 1-st channels
-	int channels[] = { 0,1 };
+    
+    unsigned char r[] = {195,194,193,191,190,189,188,187,186,185,184,183,182,181,179,178,177,176,175,174,173,172,171,170,169,167,166,165,164,163,162,161,160,159,158,157,155,154,153,152,151,150,149,148,147,146,145,143,142,141,140,139,138,137,136,135,134,133,131,130,129,128,127,126,125,125,125,125,125,125,125,125,125,125,125,125,125,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126,126};
+    
+    Mat channels[] = {Mat(256,1, CV_8U, b), Mat(256,1, CV_8U, g), Mat(256,1, CV_8U, r)};
+    Mat lut; // Create a lookup table
+    merge(channels, 3, lut);
+    
+    Mat im_color;
+    LUT(im_gray, lut, im_color);
+    
+    return im_color;
 
-	// To store the histograms
-	MatND hist_base;
-	for(int c=1;c<numOfSegments;c++)
-	{
-		if(samples[c].dims>0){
-			//convert the sample to HSV
-			cvtColor( samples[c], hsv_base, CV_BGR2HSV );
-			//calculate the histogram
-			calcHist( &hsv_base, 1, channels, Mat(), hist_base,2, histSize, ranges, true, false );
-			//normalize the histogram
-			normalize( hist_base, hist_base, 0, 1, NORM_MINMAX, -1, Mat() );
-			//append to the collection 
-			hist_bases.push_back(hist_base);
-		}else
-		{
-			hist_bases.push_back(MatND());
-		}
-		
-		hist_base.release();
-	}
-
-	//To store the similarity of histograms
-	double similarity = 0;
-	//to keep the track of already merged segments
-	vector<bool> mearged;
-	//initialize the merged segments tracker
-	for(int k = 0; k < hist_bases.size(); k++)
-	{
-		mearged.push_back(false);
-	}	
-
-	//calculate the similarity of the histograms of each pair of segments
-	for(int c=0;c<hist_bases.size();c++)
-	{
-		for(int q=c+1;q<hist_bases.size();q++)
-		{
-			//if the segment is not merged alreay
-			if(!mearged[q])
-			{
-				if(hist_bases[c].dims>0 && hist_bases[q].dims>0)
-				{
-					//calculate the histogram similarity
-					similarity = compareHist(hist_bases[c],hist_bases[q],CV_COMP_BHATTACHARYYA);
-					//if similay
-					if(similarity>0.8)
-					{
-						mearged[q]=true;
-						if(q!=c)
-						{
-							//reduce number of segments
-							newNumOfSegments--;
-							for(int i = 0; i < segments.rows; i++ )
-							{
-								for(int j = 0; j < segments.cols; j++ )
-								{
-									int index = segments.at<int>(i,j);
-									//merge the segment q with c
-									if(index==q){
-										segments.at<int>(i,j) = c;
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-	numOfSegments = newNumOfSegments;
-}
-
-Mat watershedSegment(Mat & image, int & noOfSegments)
-{	
-	//To store the gray version of the image
-	Mat gray;
-	//To store the thresholded image
-	Mat ret;
-	//convert the image to grayscale
-	cvtColor(image,gray,CV_BGR2GRAY);
-	imshow("Gray Image",gray);
-	//threshold the image
-	threshold(gray,ret,0,255,CV_THRESH_BINARY_INV+CV_THRESH_OTSU);
-	imshow("Image after OTSU Thresholding",ret);
-	//Execute morphological-open
-	morphologyEx(ret,ret,MORPH_OPEN,Mat::ones(9,9,CV_8SC1),Point(4,4),2);
-	imshow("Thresholded Image after Morphological open",ret);
-	//get the distance transformation 
-	Mat distTransformed(ret.rows,ret.cols,CV_32FC1);
-	distanceTransform(ret,distTransformed,CV_DIST_L2,3);
-	//normalize the transformed image in order to display
-	normalize(distTransformed, distTransformed, 0.0, 1, NORM_MINMAX);
-	imshow("Distance Transformation",distTransformed);
-	//threshold the transformed image to obtain markers for watershed
-	threshold(distTransformed,distTransformed,0.1,1,CV_THRESH_BINARY);
-	//Renormalize to 0-255 to further calculations
-	normalize(distTransformed, distTransformed, 0.0, 255.0, NORM_MINMAX);
-	distTransformed.convertTo(distTransformed,CV_8UC1);
-	imshow("Thresholded Distance Transformation",distTransformed);
-		
-	//calculate the contours of markers
-	int i, j, compCount = 0;
-    vector<vector<Point> > contours;
-    vector<Vec4i> hierarchy;
-	findContours(distTransformed, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE);
-
-	if( contours.empty() )
-        return Mat();
-    Mat markers(distTransformed.size(), CV_32S);
-    markers = Scalar::all(0);
-    int idx = 0;
-	//draw contours 
-    for( ; idx >= 0; idx = hierarchy[idx][0], compCount++ )
-        drawContours(markers, contours, idx, Scalar::all(compCount+1), -1, 8, hierarchy, INT_MAX);
-	
-    if( compCount == 0 )
-        return Mat();
-
-	//calculate the time taken to the watershed algorithm
-    double t = (double)getTickCount();
-	//apply watershed with the markers as seeds
-    watershed( image, markers );
-    t = (double)getTickCount() - t;
-    printf( "execution time = %gms\n", t*1000./getTickFrequency() );
-
-	//create displayable image of segments
-	Mat wshed = createSegmentationDisplay(markers,compCount);
-
-    imshow( "watershed transform", wshed );
-	noOfSegments = compCount;
-
-	//returns the segments
-	return markers;
-}
-
-Mat createSegmentationDisplay(Mat & segments,int numOfSegments,Mat & image)
-{
-	//create a new image
-	Mat wshed(segments.size(), CV_8UC3);
-	
-	//Create color tab for coloring the segments
-	vector<Vec3b> colorTab;
-    for(int i = 0; i < numOfSegments; i++ )
-    {
-        int b = theRNG().uniform(0, 255);
-        int g = theRNG().uniform(0, 255);
-        int r = theRNG().uniform(0, 255);
-
-        colorTab.push_back(Vec3b((uchar)b, (uchar)g, (uchar)r));
-    }
-	
-	//assign different color to different segments
-	for(int i = 0; i < segments.rows; i++ )
-	{
-		for(int j = 0; j < segments.cols; j++ )
-		{
-			int index = segments.at<int>(i,j);
-			if( index == -1 )	
-				wshed.at<Vec3b>(i,j) = Vec3b(255,255,255);
-			else if( index <= 0 || index > numOfSegments )
-				wshed.at<Vec3b>(i,j) = Vec3b(0,0,0);
-			else
-				wshed.at<Vec3b>(i,j) = colorTab[index - 1]; 
-		}
-	}
-
-	//If the original image available then merge with the colors of segments
-	if(image.dims>0)
-		wshed = wshed*0.5+image*0.5;
-
-	return wshed;
 }
 
 int main( int argc, char** argv )
-{
-     Mat image,frame;
-       Mat new_image = Mat::zeros( image.size(), image.type() );
+{   Mat image,fin_img;
+    image = imread("waterbody.jpg", CV_LOAD_IMAGE_COLOR);   // Read the file
 
-    image = imread(argv[1], CV_LOAD_IMAGE_COLOR);
-    
-    
-   cvtColor(image, hsv, COLOR_BGR2HSV);
-   //saturation of image	
-   for( int y = 0; y < image.rows; y++ ) {
-        for( int x = 0; x < image.cols; x++ ) {
-            for( int c = 0; c < 3; c++ ) {
-                new_image.at<Vec3b>(y,x)[c] =
-                  saturate_cast<uchar>( alpha*( image.at<Vec3b>(y,x)[c] ) + beta );
-            }
-        }
+    if(! image.data )                              // Check for invalid input
+    {
+        cout <<  "Could not open or find the image" << std::endl ;
+        return -1;
     }
-	Mat brightimage;
-	//improving brightness of image
-    new_image.convertTo(brightimage, -1, 1, 25);
-    int threshold_value = 0;
-    int threshold_type = 3;
-    int const max_value = 225;
-    int const max_BINARY_value = 225;
-	//intensity threshold to ignore low intensity value
-	threshold( src_gray, dst, threshold_value, max_BINARY_value,threshold_type );
-  int numOfSegments = 0; 
-    //water body segmentation
-    Mat segments = watershedSegment(dst,numOfSegments);
-    
-    mergeSegments(dst,segments, numOfSegments);
-      //creating  markers for water body based on segments
-    Mat wshed = createSegmentationDisplay(segments,numOfSegments);
-     
-    Mat wshedWithImage = createSegmentationDisplay(segments,numOfSegments,dst);
 
-   
-    imshow("Merged segments",wshed);
-    
-    imshow("Merged segments with image",wshedWithImage);
+   namedWindow( "Display window", CV_WINDOW_AUTOSIZE );// Create a window for display.
+                      // Show our image inside it.
 
-    return 0;
+  
+
+    // Create Matrices (make sure there is an image in input!)
+
+    Mat channel[3];
+ Mat hsv_channels[3];
+    imshow( "Original Image", image );
+
+
+    // The actual splitting.
+    split(image, channel);
+
+
+   channel[0]=Mat::zeros(image.rows, image.cols, CV_8UC1);//Set blue channel to 0
+
+    //Merging red and green channels
+  
+    merge(channel,3,image);
+    imshow("R+G", image);
+  
+
+ imwrite("dest.jpg",image);
+  //Output 1 RGB images
+ cv:imshow("RGB Image",image);
+   ///hsv conversion  
+  cvtColor(image,hsv,CV_RGB2HSV);  
+ ///Output 2 displaying HUE Image  
+  cv::imshow("hsv",hsv);
+ Mat hsv_channels[3];
+ cv::split( HSV, hsv_channels );
+ Mat gray=hsv_channels[2];
+ //We increase the brightness of image by scale of 10
+  Mat imgH = gray + Scalar(10, 10, 10);
+ //output 3 increasing brightness of image
+  cv:imshow("Bright image",imH);
+ Mat saturated;
+
+double saturation = 10;
+double scale = 1;
+
+//Saturating the gray image 
+img.convertTo(saturated, gray, scale, saturation); 
+ //Output 4 Saturated image
+ cv:imshow("Saturated image",saturated);
+ 
+ //applying pseudo coloring on the image based on pixel value(intensity)
+ Mat im_color = applyCustomColorMap(saturated);
+    //Output 5:Psedo colored image based on intensity
+    imshow("Pseudo Colored Image", im_color);
+ 
 }
